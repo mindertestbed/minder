c64e78fa88717a31b5f0f2e9f76544898c5a1bf7
yerlibilgin <myildiz83@gmail.com>
2015-10-13 11:14:45 +0300

    added rest initial files

---
+++ app/rest/model/BaseDTO.java
@@ -0,0 +1,11 @@
+package rest.model;
+
+/**
+ * @author: yerlibilgin
+ * @date: 23/09/15.
+ */
+public class BaseDTO {
+  public Long id;
+  public String shortDescription;
+  public String description;
+}
