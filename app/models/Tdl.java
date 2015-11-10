194417ee0cfa545abd50465f1f435c280a8d4b34
melis <melis@10.2.90.34>
2015-11-10 10:22:32 +0200

    Added the function getAllUnparametricTdls in TDL.java for GITB.

--- app/models/Tdl.java
+++ app/models/Tdl.java
@@ -1,8 +1,7 @@
 package models;
 
-import com.avaje.ebean.Ebean;
-import com.avaje.ebean.SqlQuery;
-import com.avaje.ebean.Model;
+import com.avaje.ebean.*;
+import com.avaje.ebean.Query;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -19,44 +18,60 @@ import java.util.List;
 @Entity
 @Table(name = "Tdl")
 public class Tdl extends Model {
-  @Id
-  public long id;
+    @Id
+    public long id;
 
-  @ManyToOne
-  @Column(nullable = false)
-  public TestCase testCase;
+    @ManyToOne
+    @Column(nullable = false)
+    public TestCase testCase;
 
-  @Column(nullable = false, length = ModelConstants.MAX_TDL_LENGTH, columnDefinition = "TEXT")
-  public String tdl;
+    @Column(nullable = false, length = ModelConstants.MAX_TDL_LENGTH, columnDefinition = "TEXT")
+    public String tdl;
 
-  @Column(nullable = false)
-  public String version;
+    @Column(nullable = false)
+    public String version;
 
-  @OneToMany
-  public List<AbstractJob> jobs;
+    @OneToMany
+    public List<AbstractJob> jobs;
 
-  public Date creationDate;
+    public Date creationDate;
 
 
-  @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-  public List<WrapperParam> parameters;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    public List<WrapperParam> parameters;
 
-  private static final Finder<Long, Tdl> find = new Finder<>(Tdl.class);
+    private static final Finder<Long, Tdl> find = new Finder<>(Tdl.class);
 
-  public static Tdl getLatestTdl(TestCase testCase) {
-    final List<Tdl> list = findByTestCase(testCase);
+    public static Tdl getLatestTdl(TestCase testCase) {
+        final List<Tdl> list = findByTestCase(testCase);
 
-    if (list.size() == 0)
-      return null;
+        if (list.size() == 0)
+            return null;
 
-    return list.get(0);
-  }
+        return list.get(0);
+    }
 
-  public static List<Tdl> findByTestCase(TestCase testCase) {
-    return find.where().eq("testCase", testCase).orderBy().desc("creationDate").findList();
-  }
+    public static List<Tdl> findByTestCase(TestCase testCase) {
+        return find.where().eq("testCase", testCase).orderBy().desc("creationDate").findList();
+    }
 
-  public static Tdl findById(Long tdlId) {
-    return find.byId(tdlId);
-  }
+    public static Tdl findById(Long tdlId) {
+        return find.byId(tdlId);
+    }
+
+    public static List<Tdl> getAllUnparametricTdls() {
+        RawSql rawSql = RawSqlBuilder.unparsed("SELECT t.* FROM tdl t LEFT JOIN wrapperparam w ON t.id = w.tdl_id WHERE w.tdl_id IS NULL")
+                .columnMapping("id", "id")
+                .columnMapping("test_case_id","testCase.id")
+                .columnMapping("tdl","tdl")
+                .columnMapping("version","version")
+                .columnMapping("creation_date","creationDate")
+                .create();
+
+        Query<Tdl> query = Ebean.find(Tdl.class);
+        query.setRawSql(rawSql);
+        List<Tdl> tdlList = query.findList();
+
+        return tdlList;
+    }
 }
